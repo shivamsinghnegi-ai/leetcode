10a2875c876895f1fb2edd48d1c766cf37e4a0cc
shivam Singh Negi <shivamsinghnegi2006@gmail.com>
2026-02-22 20:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (78.57%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max1 = -1, max2 = -1;
+        int index = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                index = i;
+            } 
+            else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+        if (max1 >= max2*2)
+            return index;
+
+        return -1; 
+    }
+};
