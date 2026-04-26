3d0675f73c2600cbe73f1826a21d577292a4db1c
shivam Singh Negi <shivamsinghnegi2006@gmail.com>
2026-04-26 20:48:09 +0530

    Time: 12 ms (93.45%) | Memory: 17.5 MB (53.21%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> output;
-        for(int i=0; i<n-2; i++){
-            for(int j=i+1; j<n-1; j++){
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
                 long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
                 int low = j+1, high = n-1;
                 while(low < high){
